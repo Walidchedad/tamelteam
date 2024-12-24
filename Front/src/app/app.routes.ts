cabab31db608a3c4191b3b9ca12596a919e7eaa6
Walidchedad <walidchedad65@gmail.com>
2024-12-24 17:06:44 +0100

    Update Project

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { NavbarComponent } from './component/navbar/navbar.component';
 import { AcceuillComponent } from './component/acceuill/acceuill.component';
+import { LoginComponent } from './component/login/login.component';
 
 export const routes: Routes = [
+    {path:'Login', component: LoginComponent},
     {path:'',component:NavbarComponent,children:[
         {path:'Acceuil',component:AcceuillComponent},
+        {path:'',component:AcceuillComponent},
+        {path:'Login',component:LoginComponent}
     ]},
 ];
--- Front/src/app/component/login/login.component.ts
+++ Front/src/app/component/login/login.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [RouterOutlet, CommonModule, FormsModule, ReactiveFormsModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
